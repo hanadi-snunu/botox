806c3656d9adbb3a73c9be18a2272c4e4bff9f95
94hansnu <94hansnu@gafe.molndal.se>
2024-10-06 01:24:40 +0200

    navbar starting

--- botox/src/component/layout/Navbar.js
+++ botox/src/component/layout/Navbar.js
@@ -1,36 +1,4 @@
-/*import React from "react";
-import { Link } from 'react-router-dom';
-import '../css/Navbar.css';
 
-const Navbar = () => {
-    return(
-        <header className="header-container">
-      <div className="navbar-top">
-        <div className="logo">
-        <Link to="/">
-            <h1>House</h1>
-          </Link>
-        </div>
-        <div className="contact-info">
-          <span>tel: 070 - 20 40 998</span>
-          <button className="book-btn">Boka tid</button>
-        </div>
-      </div>
-      <nav className="navbar-bottom">
-        <ul className="nav-links">
-          <li><Link to="/Behandlingsutbud">BEHANDLINGSUTBUD</Link></li>
-          <li><Link to="/Priser">PRISER</Link></li>
-          <li><Link to="/Omoss">OM OSS</Link></li>
-          <li><Link to="/Utbildningar">UTBILDNINGAR</Link></li>
-          <li><Link to="/Kontakt">KONTAKT</Link></li>
-          <li><Link to="/HittaBehandling">HITTA RÄTT BEHANDLING</Link></li>
-        </ul>
-      </nav>
-    </header>
-    );
-}
-
-export default Navbar;*/
 
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
