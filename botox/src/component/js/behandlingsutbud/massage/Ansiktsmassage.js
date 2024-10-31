767dbec1221d97f4a1c7b923bb4e3fb2678d4d6a
94hansnu <94hansnu@gafe.molndal.se>
2024-10-31 10:07:57 +0100

    --

--- botox/src/component/js/behandlingsutbud/massage/Ansiktsmassage.js
+++ botox/src/component/js/behandlingsutbud/massage/Ansiktsmassage.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import massage1 from '../../../img/massage1.jpg';
-
+import massage3 from '../../../img/massage3.jpg';
+import { Link } from 'react-router-dom';
 import '../../../css/Massage.css';
+import ConsultationImage from '../../../img/piccc.jpg';
 
 
 const AnsiktsMassage = () => {
@@ -16,6 +18,33 @@ const AnsiktsMassage = () => {
         
       </div>
     </header>
+    <div>
+
+    <div className="relax-treatment-card">
+<img src={massage3} alt="Behandling" className="relax-treatment-image" />
+<div className="relax-treatment-content">
+    <h3 className="relax-treatment-title">Ansiktsmassage</h3>
+    <p className="relax-treatment-description">
+    Unna dig själv en lyxig stund med en av våra mest omtyckta behandlingar! En ansiktsmassage främjar cirkulationen och ger en härlig känsla av avkoppling. Vår skickliga hudterapeut kommer dessutom att förgylla din upplevelse med en avslappnande massage för halsen och dekolletaget.
+    </p>
+   
+</div>
+</div>
+</div>
+    {/* Consultation Section */}
+    <div className="consultation-section" style={{ backgroundImage: `url(${ConsultationImage})` }}>
+        <div className="consultation-overlay">
+          <div className="consultation-content">
+            <h2 className="consultation-title">Boka en kostnadsfri digital konsultation</h2>
+            <p className="consultation-description">
+              Vi erbjuder kostnadsfria digitala konsultationer för att hjälpa dig att förstå vilken behandling som passar dig bäst. Vi svarar på alla dina frågor och hjälper dig att boka den tid som passar dig.
+            </p>
+            <Link to="/Bokatid" className="consultation-button">
+              Boka nu
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
  )
 }
--- botox/src/component/js/behandlingsutbud/massage/KlippkortX5.js
+++ botox/src/component/js/behandlingsutbud/massage/KlippkortX5.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import massage1 from '../../../img/massage1.jpg';
-
+import massage4 from '../../../img/massage4.jpg';
+import { Link } from 'react-router-dom';
 import '../../../css/Massage.css';
+import ConsultationImage from '../../../img/piccc.jpg';
 
 
 const KlippkortX5 = () => {
@@ -16,6 +18,38 @@ const KlippkortX5 = () => {
         
       </div>
     </header>
+
+    <div>
+
+<div className="relax-treatment-card">
+<img src={massage4} alt="Behandling" className="relax-treatment-image" />
+<div className="relax-treatment-content">
+    <h3 className="relax-treatment-title">Klippkort 5X</h3>
+    <p className="relax-treatment-description">
+    Massage Klippkort x5gg. Känner du spänningarna byggas upp i nacke och axlar? Då är det dags att ta en paus och unna dig själv en avkopplande massage.
+
+Våra erfarna massageterapeuter är redo att hjälpa dig lösa upp muskelspänningar och få dig att slappna av med en kort men effektiv massage som fokuserar på problemområdena. Dessutom kommer du även unna dig en avslappnande skalpmassage för att ytterligare öka ditt välbefinnande. 
+
+Boka din behandling redan idag och upplev ren njutning!
+    </p>
+   
+</div>
+</div>
+</div>
+    {/* Consultation Section */}
+    <div className="consultation-section" style={{ backgroundImage: `url(${ConsultationImage})` }}>
+        <div className="consultation-overlay">
+          <div className="consultation-content">
+            <h2 className="consultation-title">Boka en kostnadsfri digital konsultation</h2>
+            <p className="consultation-description">
+              Vi erbjuder kostnadsfria digitala konsultationer för att hjälpa dig att förstå vilken behandling som passar dig bäst. Vi svarar på alla dina frågor och hjälper dig att boka den tid som passar dig.
+            </p>
+            <Link to="/Bokatid" className="consultation-button">
+              Boka nu
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
  )
 }
